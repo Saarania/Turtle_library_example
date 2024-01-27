b39d4b1e019b4125f6bfb408a1ccc68aa3eab164
Saramia <sara.praksova@gmail.com>
2024-01-27 12:47:42 +0100

    Uploading files to just created repository

---
+++ scripts/main.py
@@ -0,0 +1,12 @@
+import turtle
+t = turtle.Turtle();
+t.color("red")
+t.begin_fill()
+t.fillcolor("red")
+t.left(140)
+t.forward(180)
+t.circle(-90,200)
+t.setheading(60)
+t.circle(-90,200)
+t.forward(180)
+t.end_fill()
